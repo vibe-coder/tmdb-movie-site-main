9f0cef88858066a0b652365c524340bb0fd09ff5
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-05-30 10:59:52 +0100

    Setting up Nav bar for mobile

--- tmdb-movie-site/app/Nav.jsx
+++ tmdb-movie-site/app/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import "./sass/Nav.scss"
 
+import {Raleway} from "next/font/google"
+
+const raleway = Raleway({
+  weight: '400',
+  subsets: ['latin'],
+});
+
 const Nav = () => {
   return (
-    <div className='nav-container'>This is Nav Bar</div>
+    <div className='nav-container'>
+      <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;1,100&display=swap" rel="stylesheet"></link>
+      <div className='nav-wrapper'>
+        <h1 className={raleway.className}>MovieSpace</h1>
+        <button>Category</button>
+      </div>
+    </div>
   )
 }
 
--- tmdb-movie-site/app/page.js
+++ tmdb-movie-site/app/page.js
@@ -1,11 +1,14 @@
 import Page from './home/page'
 import Nav from './Nav'
 
+
 export default function Home() {
   return (
-    <div>
-      <Nav/>
-      <Page/>
-    </div>
+      <div>
+        <div>
+          <Nav/>
+          <Page/>
+        </div>
+      </div>
   )
 }
