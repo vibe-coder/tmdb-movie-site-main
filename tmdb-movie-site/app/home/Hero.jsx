b93bd8d1dfc50cde7a998c3a88dcb0901a8050d7
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-05-30 11:59:22 +0100

    Styling Hero Section

--- tmdb-movie-site/app/home/Hero.jsx
+++ tmdb-movie-site/app/home/Hero.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import "../sass/Home/Hero.scss"
+import IronMan from "../images/wallpaperflare.com_wallpaper (23).jpg"
 
 const Hero = () => {
   return (
-    <div className='hero-container'>Hero</div>
+    <div className='hero-container'>
+      <img src={IronMan} alt='Iron Man'/>
+      <h1>Hero</h1>
+
+    
+    </div>
   )
 }
 
