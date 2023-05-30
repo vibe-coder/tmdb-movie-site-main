75af3088d29101ef1fdc1781789a8e8ead397101
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-05-30 19:48:13 +0100

    Adjusting the Hero section

--- tmdb-movie-site/app/home/Hero.jsx
+++ tmdb-movie-site/app/home/Hero.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 import "../sass/Home/Hero.scss"
 import IronMan from "../images/Iron-Man.jpg"
 import Image from 'next/image'
+import { IoChevronDownSharp } from "react-icons/io5";
+
 
 const Hero = () => {
   return (
     <div className='hero-container'>
-      <Image className='backdrop_image' src={IronMan} alt='Iron Man' />
+      <Image className='backdrop-image' src={IronMan} alt='Iron Man' />
       
       <div className='item-wrapper'>
+        <p>Welcome back!</p>
         <h1>MovieSapce</h1>
         <h2>Unlimited movies, TV shows, and more.</h2>
         <button>Ready to explore?</button>
+        <IoChevronDownSharp className='arrow-icon'/>
       </div>
      </div>
   )
