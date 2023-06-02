156bf5627a5f938e229328ec199348336b696b4e
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-06-02 10:19:56 +0100

    Styling Footer Section

--- tmdb-movie-site/app/Footer.jsx
+++ tmdb-movie-site/app/Footer.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import "./sass/Home/Footer.scss"
+import Image from 'next/image'
+import TMDB from "./images/tmdb-logo.png"
 
 const Footer = () => {
   return (
-    <div className='footer-container'>Footer</div>
+    <div className='footer-container'>
+      <p>Developed with:</p>
+      <div className='image-wrapper'><Image src={TMDB} alt='TMDB Logo' /></div>
+      <p>The Vibecoder @ 2023</p>
+    </div>
   )
 }
 
