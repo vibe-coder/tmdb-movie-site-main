61a5cf16c8fb79362c7fba85aa51bae6b1415cfe
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-06-02 12:32:05 +0100

    Creating category page

---
+++ tmdb-movie-site/app/category/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../sass/Category/Page.scss"
+
+const Page = () => {
+  return (
+    <div className='category-container'>Page</div>
+  )
+}
+
+export default Page
