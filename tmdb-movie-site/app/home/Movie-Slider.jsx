88f20d688f08a177b8267423145998bc6621afaf
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-06-02 11:36:44 +0100

    Adjusting Movie Slider

--- tmdb-movie-site/app/home/Movie-Slider.jsx
+++ tmdb-movie-site/app/home/Movie-Slider.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import "../sass/Home/Movie-Slider.scss"
-import IronMan from "../images/Iron-Man.jpg"
+import IronMan from "../images/ironman-silver.jpg"
 import Image from 'next/image'
+import { IoChevronBackSharp } from "react-icons/io5";
+import { IoChevronForwardSharp } from "react-icons/io5";
+
 
 const MovieSlider = () => {
   return (
@@ -9,16 +12,16 @@ const MovieSlider = () => {
     <Image priority={true} className='backdrop_image' src={IronMan} alt='Iron Man' />
     
     <div className='item-wrapper'>
-      {/* <div className='left'>L</div> */}
+      <div className='left'><IoChevronBackSharp/></div>
 
       <div className='center'>
-        <p className='now-showing'>Now Showing</p>
-        <h1 className='movie-title'>Iron Man</h1>
+        <h1 className='now-showing'>Now Showing</h1>
+        <h2 className='movie-title'>Iron Man</h2>
         <p className='movie-description'>When Tony Stark, an industrialist, is captured, he constructs a high-tech armoured suit to escape. Once he manages to escape, he decides to use his suit to fight against evil forces to save the world.</p>
         <p className='movie-date'>Realeased: 2023-05-17</p>
       </div>
 
-      {/* <div className='right'>R</div> */}
+      <div className='right'><IoChevronForwardSharp/></div>
     </div>
    </div>
   )
