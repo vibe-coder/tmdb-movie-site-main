53c7c724eb783222629540fa1e7824e9d7daca2e
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-06-04 20:26:05 +0100

    Mapping through data

--- tmdb-movie-site/app/category/Movie-Genre-Data.js
+++ tmdb-movie-site/app/category/Movie-Genre-Data.js
@@ -1,92 +1,92 @@
 const Data = [
   {
     name: 'ACTION',
-    img: "../images/action.jpg"
+    backdrop: "../images/action.jpg"
   },
 
   {
     name: 'ADVENTURE',
-    img: "../images/action.jpg"
+    backdrop: "../images/action.jpg"
   },
 
   {
     name: 'ANIMATION',
-    img: "../images/action.jpg"
+    backdrop: "../images/action.jpg"
   },
 
   {
     name: 'COMEDY',
-    img: "../images/action.jpg"
+    backdrop: "../images/action.jpg"
   },
 
   {
     name: 'CRIME',
-    img: "../images/action.jpg"
+    backdrop: "../images/action.jpg"
   },
 
   {
     name: 'DOCUMENTARY',
-    img: "../images/action.jpg"
+    backdrop: "../images/action.jpg"
   },
 
   {
     name: 'DRAMA',
-    img: "../images/action.jpg"
+    backdrop: "../images/action.jpg"
   },
 
   {
     name: 'FAMILY',
-    img: "../images/action.jpg"
+    backdrop: "../images/action.jpg"
   },
 
   {
     name: 'FANTASY',
-    img: "../images/action.jpg"
+    backdrop: "../images/action.jpg"
   },
 
   {
     name: 'HISTORY',
-    img: "../images/action.jpg"
+    backdrop: "../images/action.jpg"
   },
 
   {
     name: 'HORROR',
-    img: "../images/action.jpg"
+    backdrop: "../images/action.jpg"
   },
 
   {
     name: 'MUSIC',
-    img: "../images/action.jpg"
+    backdrop: "../images/action.jpg"
   },
 
   {
     name: 'MYSTERY',
-    img: "../images/action.jpg"
+    backdrop: "../images/action.jpg"
   },
 
   {
     name: 'ROMANCE',
-    img: "../images/action.jpg"
+    backdrop: "../images/action.jpg"
   },
 
   {
     name: 'SCIENCE FICTION',
-    img: "../images/action.jpg"
+    backdrop: "../images/action.jpg"
   },
 
   {
     name: 'THRILLER',
-    img: "../images/action.jpg"
+    backdrop: "../images/action.jpg"
   },
 
   {
     name: 'WAR',
-    img: "../images/action.jpg"
+    backdrop: "../images/action.jpg"
   },
 
   {
     name: 'WESTERN',
-    img: "../images/action.jpg"
+    backdrop: "../images/action.jpg"
   },
 ]
 
--- tmdb-movie-site/app/category/Movie-Genre.jsx
+++ tmdb-movie-site/app/category/Movie-Genre.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import "../sass/Category/Movie-Genre.scss"
-import Data from './Movie-Genre-Data'
+import Image from 'next/image'
 
 const MovieGenre = (prop) => {
   return (
     <button className="genre">
-    <Image className="genre-image" src={Data.img} alt="back drop"/>
-    <div className="genre-category">
-      <h1>{Data.name}</h1>
-    </div>
-  </button>
+       <Image className="genre-image" 
+       src={prop.backdrop} 
+       alt="back drop" 
+       width={200} 
+       height={200}/>
+       <div className="genre-category">
+         <h1>{prop.name}</h1>
+       </div>
+    </button>
   )
 }
 
