61608872513e7c5bae5375363d13d00c773e66a6
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-06-26 14:17:09 +0100

    Styling action genre section

--- tmdb-movie-site/app/action/page.jsx
+++ tmdb-movie-site/app/action/page.jsx
@@ -1,11 +1,72 @@
+"use client"
+
 import React from 'react'
 import "./Action.scss"
+import Image from 'next/image'
+import { useState, useEffect } from 'react'
+import backdrop from "../images/action-backdrop.jpg"
 
 function page() {
+
+
+  const [actionMovies, getActionMovies] = useState([])
+
+  const fetchActionMovies = () => {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZjg4MjY0YTZiZTI2ZWRjODlhNzY5NTJiODM2Y2FmMCIsInN1YiI6IjY0NzBiYjdlNTQzN2Y1MDE0NzVmMWEwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5kRWVCmYbyP86pKTuEuJAL4UuPTOpA0YpR2It5mTyWU'
+      }
+    };
+    
+    fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=Action', options)
+      .then(response => response.json())
+      .then(data => {
+
+        console.log(data)
+        const actionMoviesResult = data.results.slice(0, -2);
+
+        console.log(actionMoviesResult)
+
+        getActionMovies(actionMoviesResult)
+
+      })
+      .catch(err => console.error(err));
+  
+  }
+
+  useEffect(()=> {
+    fetchActionMovies()
+  }, [])
+                    
+  let imgPref = "https://image.tmdb.org/t/p/w200/"
+
+
   return (
     <div className='action-container'>
-      <div>
-        This is action
+
+      <div className='backdrop'>
+        <div className='title-background'>
+          <h1>Action</h1>
+        </div>
+        <Image priority={true} className='backdrop-poster' src={backdrop} alt="backdrop" height={450} width={350} />        
+      </div>
+
+      
+      <div className='action-movies-wrapper'>
+
+        {
+          actionMovies.map((actionMovieData) => {
+            return(
+              <div className='action-movie-box' key={actionMovieData.id}>
+              <Image priority={true} className='action-poster' src={imgPref + actionMovieData.poster_path} alt={actionMovieData.original_title} fill={true} sizes={100} />        
+            </div>
+            )
+          })
+        }
+
+
       </div>
     </div>
   )
